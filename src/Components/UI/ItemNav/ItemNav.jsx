3e139497cd64b580a7ab3a3242e8fd158a991926
Andrea gaona 14 <andreitagaona05@gmail.com>
2025-03-21 06:59:12 -0500

    Creación de ItemNav.jsx

--- src/Components/UI/ItemNav/ItemNav.jsx
+++ src/Components/UI/ItemNav/ItemNav.jsx
@@ -1,4 +1,4 @@
-/*import React from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 export const ItemNav = ({ Word, link = '', classItem}) => {
@@ -10,4 +10,3 @@ export const ItemNav = ({ Word, link = '', classItem}) => {
     </li>
   )
 }
-  */
