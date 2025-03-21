70aa920e04c00a0f88ee1c64dd139f4d1024ea0a
Andrea gaona 14 <andreitagaona05@gmail.com>
2025-03-21 06:36:14 -0500

    Creación de Home.jsx

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-/*import { Profile } from "../../Layouts/Profile/Profile";
+import { Profile } from "../../Layouts/Profile/Profile";
 import { AnimatedContent } from "../../UI/AnimatedContent/AnimatedContent";
 import ImgHome from "../../../assets/img/ImgHome.jpeg";
 import "./Home.css";
@@ -19,4 +19,3 @@ export const Home = () => {
     </>
   )
 }
-*/
