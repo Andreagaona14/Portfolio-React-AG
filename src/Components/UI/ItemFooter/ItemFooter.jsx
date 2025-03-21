3b98238ff85c1e8503ac583c1c5dc2c9e009c74e
Andrea gaona 14 <andreitagaona05@gmail.com>
2025-03-21 06:55:57 -0500

    Creación de ItemFooter.jsx

--- src/Components/UI/ItemFooter/ItemFooter.jsx
+++ src/Components/UI/ItemFooter/ItemFooter.jsx
@@ -1,8 +1,7 @@
-/*import React from 'react'
+import React from 'react'
 
 export const ItemFooter = ({icon: Icon, link}) => {
   return (
    <li><a href={link} target="_blank" rel="noopener noreferrer"><Icon /></a></li>
   )
 }
-  */
